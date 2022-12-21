276b036a6b5e618b34ab8e729ba92a1c30d8a99d
Mateus Borges <borgesm.work@gmail.com>
2022-12-21 13:57:50 -0300

    feat: create user context

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './assets/styles/GlobalStyle.js';
 import Header from './components/Header.js';
 import Loading from './components/Loading';
+import { UserProvider } from './contexts/UserContext.js';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path='/' element={<Loading />} />
-        <Route path='/shortenUrl' element={<Loading />} />
-        <Route path='/signUp' element={<Loading />} />
-        <Route path='/signIn' element={<Loading />} />
-      </Routes>
+      <UserProvider>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Loading />} />
+          <Route path='/shortenUrl' element={<Loading />} />
+          <Route path='/signUp' element={<Loading />} />
+          <Route path='/signIn' element={<Loading />} />
+        </Routes>
+      </UserProvider>
     </BrowserRouter>
   );
 }
